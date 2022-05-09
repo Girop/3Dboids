eb5400224cf710bbf411a12c65a997c6598988d3
Girop <65816493+Girop@users.noreply.github.com>
2022-05-09 11:44:36 +0200

    boids moving

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// initialize 3d environment
 const CAGESIZE = 600
-const BOID_QUANTITY = 30
+const BOID_QUANTITY = 100
 const boids = []
+
+const collisionForce = 0.3
+const directionForce = 0.3
+const centerForce = 0.16
 
 const scene = new THREE.Scene()
 
@@ -32,7 +35,7 @@ class Cage {
         scene.add(this.mesh)
     }
 }
-const cage = new Cage(scene)
+// const cage = new Cage(scene)
 
 class Boid {
     static boidIndex = 0
@@ -49,10 +52,9 @@ class Boid {
             Math.floor(Math.random() * 100 - 50),
             Math.floor(Math.random() * 100 - 50)
         )
-        // this.velocity = new THREE.Vector3().random().normalize()
-        this.velocity = new THREE.Vector3()
-        this.acceleration = new THREE.Vector3()
-        console.log(this.velocity)
+
+        this.velocity = new THREE.Vector3().random().normalize()
+        this.acceleration = new THREE.Vector3(0,0,0)
         this.mesh = new THREE.Mesh(
             new THREE.ConeGeometry(4, 20, 32),
             new THREE.MeshBasicMaterial({ color: 0x0000aa })
@@ -61,35 +63,66 @@ class Boid {
         scene.add(this.mesh)
     }
 
-    nearbyCheck(){
-        for(let boid of boids){
-            const distance = this.position.distanceTo(boid.distance)
-            if(this.index != boid.index && distance < this.viewDistance){
+    nearbyCheck() {
+        this.nearby = []
+
+        for (let boid of boids) {
+            const distance = this.position.distanceTo(boid.position)
+            if (this.index != boid.index && distance < this.viewDistance) {
                 this.nearby.push(boid)
             }
         }
     }
 
-    coherence() {
+    avoidCollision() {
+        const collisionVector = new THREE.Vector3()
+        this.nearby.forEach(boid => {
+            const distance = this.position.distanceTo(boid.position)
+            const boidToPosVec = new THREE.Vector3()
+            boidToPosVec.copy(this.position)
+            boidToPosVec.sub(boid.position)
+            boidToPosVec.divideScalar(distance)
 
+            collisionVector.add(boidToPosVec)
+        })
+        return collisionVector.normalize().multiplyScalar(collisionForce)
     }
 
-    separation() {
-
+    matchDirection() {
+        const directionVector = new THREE.Vector3()
+        this.nearby.forEach(boid => {
+            directionVector.add(boid.position)
+        })
+        return directionVector.normalize().multiplyScalar(directionForce)
     }
 
-    aligment() {
+    centerFlock() {
+        const avgPosition = new THREE.Vector3()
+        this.nearby.forEach(boid => {
+            avgPosition.add(boid.position)
+        })
+        avgPosition.divideScalar(this.nearby.length)
 
+        return avgPosition.sub(this.position).multiplyScalar(centerForce)
     }
 
-    live(){
+    live() {
         this.nearbyCheck()
-        console.log(this.nearby)
-        // kinematics of boid
+        // Adding flock bechaviour effects
+        // avoid collision too weak 
+        this.acceleration.add(this.avoidCollision())
+        this.acceleration.add(this.matchDirection())
+        this.acceleration.add(this.centerFlock())
+        // movement of boid
         this.velocity.add(this.acceleration)
-        this.velocity.clampLength(this.velocity)
+        this.velocity.clampLength(0,3)
         this.position.add(this.velocity)
-        this.acceleration.set(0,0,0)
+        this.acceleration.multiplyScalar(0)
+        // graphical stuff
+        this.mesh.position.add(this.velocity)
+        // where to lookAt ? velocity / acceleration / or something  better ?
+        // this.mesh.lookAt()
+
     }
 }
 
@@ -101,11 +134,9 @@ for (let i = 0; i < BOID_QUANTITY; i++) {
 // event loop
 function animate() {
     requestAnimationFrame(animate)
-
-    for(let boid of boids){
+    for (let boid of boids) {
         boid.live()
     }
-
     control.update()
     renderer.render(scene, camera)
 }
